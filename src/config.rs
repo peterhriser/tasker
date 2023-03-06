d23813b83ffed33c0a83a759d1a866b7286bdd68
peterhriser <peterhriser@gmail.com>
2023-03-06 15:22:09 -0500

    working with clap hints

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,4 @@
+use clap::ArgMatches;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::fmt;
@@ -37,37 +38,35 @@ pub struct CmdArg {
     pub name: String,
     #[serde(default)]
     pub default: Option<String>,
-    #[serde(default)]
-    pub choices: Option<Vec<String>>,
+    #[serde(rename = "type")]
+    pub arg_type: String,
 }
 impl CmdArg {
-    pub fn is_input_usable(&self, input: String) -> bool {
-        match &self.choices {
-            Some(choices) => return choices.contains(&input),
-            None => return true,
-        }
-    }
     fn is_required(&self) -> bool {
         if self.default.is_none() {
             return true;
         }
         return false;
     }
-    fn get_default(&self) -> String {
-        match &self.default {
-            Some(def) => return def.to_owned(),
-            None => {
-                panic!("Attempting to access non-existent default")
-            }
-        };
+    fn get_clap_arg(&self) -> clap::Arg {
+        let name_owned = self.name.to_owned();
+        if !self.is_required() {
+            let default = self.default.to_owned().unwrap();
+            return clap::Arg::new(name_owned).default_value(default);
+        } else {
+            return clap::Arg::new(name_owned).required(true);
+        }
     }
 }
 
 // task file command is a single defined command stanza from a config
 #[derive(Debug, Deserialize)]
 pub struct TaskStanza {
+    #[serde(rename(deserialize = "cmd"))]
     pub command_template: String,
+    #[serde(rename(deserialize = "args"))]
     pub command_args: Vec<CmdArg>,
+    pub description: Option<String>,
 }
 
 impl TaskStanza {
@@ -82,60 +81,35 @@ impl TaskStanza {
         }
         return lookup_map;
     }
-}
 
-// Config File made from assembling above structs
-#[derive(Debug, Deserialize)]
-pub(crate) struct Config {
-    // TODO: use these when filling in vars
-    pub globals: HashMap<String, AllowedVarTypes>,
-    pub commands: HashMap<String, TaskStanza>,
-}
-impl Config {
-    pub fn get_task_from_name(&self, name: &str) -> Option<&TaskStanza> {
-        return self.commands.get(name);
+    fn create_clap_subcommand(&self, name: String) -> clap::Command {
+        let mut arg_vector: Vec<clap::Arg> = vec![];
+        for arg in &self.command_args {
+            let new_arg = arg.get_clap_arg();
+            arg_vector.push(new_arg)
+        }
+        let about = self.description.to_owned().unwrap_or_default();
+        let base_command = clap::Command::new(name)
+            .about(about)
+            .arg_required_else_help(true)
+            .args(arg_vector);
+        return base_command;
     }
-}
 
-// Staged Task is tying the task stanza to arguments input by users
-pub struct StagedTask<'a> {
-    pub(crate) selected_command: &'a TaskStanza,
-    pub(crate) command_inputs: Vec<String>,
-}
-impl StagedTask<'_> {
-    pub fn create_command_string(&self) -> String {
-        let mut cmd_raw = self.selected_command.command_template.to_string();
-        let replacer_map = self.selected_command.create_arg_replace_hashmap();
-        let identified_inputs = self.match_inputs_to_args();
-        for (name, value) in identified_inputs {
-            let replace_string = replacer_map.get(&name).unwrap();
-            cmd_raw = cmd_raw.replace(replace_string, &value);
+    pub fn create_command_string(&self, clap_inputs: ArgMatches) -> String {
+        let mut cmd_raw = self.command_template.to_string();
+        let replacer_map = self.create_arg_replace_hashmap();
+        for arg in &self.command_args {
+            let replace_string = replacer_map.get(&arg.name).unwrap();
+            let input_string: String = clap_inputs
+                .get_one::<String>(&arg.name)
+                .unwrap()
+                .to_string();
+            cmd_raw = cmd_raw.replace(replace_string, &input_string);
         }
         return cmd_raw;
     }
-    fn match_inputs_to_args(&self) -> HashMap<String, String> {
-        let mut parameter_to_value_map: HashMap<String, String> = HashMap::new();
-        let max_len = self.command_inputs.len();
-        for i in 0..max_len {
-            let potential_arg = &self.selected_command.command_args[i];
-            let potential_input = &self.command_inputs[i];
-            if potential_arg.is_input_usable(potential_input.to_string()) {
-                parameter_to_value_map.insert(
-                    potential_arg.name.to_string(),
-                    self.command_inputs[i].to_string(),
-                );
-            }
-        }
-        let remaining_args = &self.selected_command.command_args[max_len..];
-        for arg in remaining_args {
-            if arg.is_required() {
-                panic!("Missing {}", arg.name);
-            } else {
-                parameter_to_value_map.insert(arg.name.to_string(), arg.get_default());
-            }
-        }
-        return parameter_to_value_map;
-    }
+
     fn parse_command_from_string(&self, command_str: String) -> Result<Command, io::Error> {
         let mut parts = command_str.split_whitespace();
         let command_name = parts.next().expect("no command specified");
@@ -146,14 +120,14 @@ impl StagedTask<'_> {
         Ok(cmd)
     }
 
-    fn generate_command_with_args(&self) -> Result<Command, io::Error> {
-        let cmd_string = self.create_command_string();
+    fn generate_command_with_args(&self, inputs: ArgMatches) -> Result<Command, io::Error> {
+        let cmd_string = self.create_command_string(inputs);
         self.parse_command_from_string(cmd_string)
     }
 
-    fn call_command(&self) -> Result<ChildStdout, io::Error> {
+    fn call_command(&self, clap_inputs: ArgMatches) -> Result<ChildStdout, io::Error> {
         let result = self
-            .generate_command_with_args()
+            .generate_command_with_args(clap_inputs)
             .unwrap()
             .stdout(Stdio::piped())
             .stderr(Stdio::piped())
@@ -165,8 +139,8 @@ impl StagedTask<'_> {
 
         Ok(result)
     }
-    pub fn stream_command(&self) -> Result<(), io::Error> {
-        let cmd_results = self.call_command().unwrap();
+    pub fn stream_command(&self, clap_inputs: ArgMatches) -> Result<(), io::Error> {
+        let cmd_results = self.call_command(clap_inputs).unwrap();
         let reader = io::BufReader::new(cmd_results);
 
         io::BufRead::lines(reader).for_each(|line| {
@@ -176,5 +150,37 @@ impl StagedTask<'_> {
 
         println!("Completed Task!");
         Ok(())
+    }
+}
+
+// Config File made from assembling above structs
+#[derive(Deserialize)]
+pub(crate) struct Config {
+    // TODO: use these when filling in vars
+    #[serde(rename(deserialize = "globals"))]
+    pub _globals: HashMap<String, AllowedVarTypes>,
+    pub commands: HashMap<String, TaskStanza>,
+}
+impl Config {
+    pub fn get_task_by_name(&self, name: &str) -> Option<&TaskStanza> {
+        return self.commands.get(name);
+    }
+    pub fn new(file_path: String) -> Result<Config, std::io::Error> {
+        let file = std::fs::File::open(file_path).expect("Could not open file");
+        let deserialized_config: Config =
+            serde_yaml::from_reader(file).expect("Could not read values.");
+        Ok(deserialized_config)
+    }
+    pub fn create_clap_command(&self) -> clap::Command {
+        let mut task_vector: Vec<clap::Command> = vec![];
+        for (name, task) in &self.commands {
+            let new_command = task.create_clap_subcommand(name.to_string());
+            task_vector.push(new_command);
+        }
+        let base_command = clap::Command::new("tasker")
+            .about("about")
+            .arg_required_else_help(true)
+            .subcommands(task_vector);
+        return base_command;
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod config;
 mod task;
 use clap::Parser;
-use config::StagedTask;
+use config::Config;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None, arg_required_else_help(true))]
@@ -13,29 +13,35 @@ struct Args {
     task_args: Vec<String>,
 }
 
-fn load_task_file(file_path: String) -> Result<config::Config, std::io::Error> {
-    let f = std::fs::File::open(file_path).expect("Could not open file");
-    let d: config::Config = serde_yaml::from_reader(f).expect("Could not read values.");
-    Ok(d)
-}
-
 fn main() {
-    let args = Args::parse();
-    let config: config::Config = load_task_file(args.config).unwrap();
-    let selected_command = match config.get_task_from_name(&args.command) {
-        Some(tsk) => tsk,
-        None => {
-            show_help_statement(&config);
-            std::process::exit(1)
-        }
-    };
-    let staged: StagedTask = StagedTask {
-        selected_command: selected_command,
-        command_inputs: args.task_args,
-    };
-    let _ = staged.stream_command();
-}
+    // let args = Args::parse();
+    // let mut cmd = Args::command();
+    // cmd.print_help();
+    let config = Config::new("config.yaml".to_string()).unwrap();
+    let cmd = config.create_clap_command();
+    let inputs = cmd.get_matches();
+    // we can be confident in unwraps since CLAP can handle most errors on read above
+    let subcmd = inputs.subcommand_name().unwrap();
+    let chosen_command = config.get_task_by_name(subcmd).unwrap();
+    let (_, subcmd_struct) = inputs.subcommand().unwrap();
+    let subcmd_inputs = subcmd_struct.to_owned();
+    let _ = chosen_command.stream_command(subcmd_inputs);
 
-fn show_help_statement(config: &config::Config) {
-    println!("Help! {:?}", config)
+    // let staged: StagedTask = StagedTask {
+    //     selected_command: chosen_command,
+    //     command_inputs: inputs.get_raw(id)
+    // };
+    // println!("{:?}", x)
+    // let selected_command = match config.get_task_by_name(&args.command) {
+    //     Some(tsk) => tsk,
+    //     None => {
+    //         &config.show_help_statement();
+    //         std::process::exit(1)
+    //     }
+    // };
+    // let staged: StagedTask = StagedTask {
+    //     selected_command: selected_command,
+    //     command_inputs: args.task_args,
+    // };
+    // let _ = staged.stream_command();
 }
